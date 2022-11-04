6f7cd64ce202ec0418aaff381bb91b74c91627f3
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2022-11-04 02:10:29 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 function createGame(pais1, hora, pais2) {
   return `
   <li>
-    <img src="svg/icon=${pais1}.svg" alt="bandeira ${pais1}">
+    <abbr title="${pais1}"><img src="svg/icon=${pais1}.svg" alt="bandeira ${pais1}"></abbr>
       <strong>${hora}</strong>
-    <img src="svg/icon=${pais2}.svg" alt="bandeira ${pais2}">
+    <abbr title="${pais2}"><img src="svg/icon=${pais2}.svg" alt="bandeira ${pais2}"></abbr>
   </li>
   `
 }
-
+let delay = -0.4
 function createCard(date, day, games) {
+  delay += 0.4
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
       <h2>${date} <span>${day}</span></h2>
         <ul>
               ${games}
@@ -19,36 +20,31 @@ function createCard(date, day, games) {
 `
 }
 
-document.querySelector("#app").innerHTML = `
-      <header>  
-        <img src="svg/logo-nlw-copa.svg" alt="logo da NLW">
-      </header>
-      <main class="cards">
-        ${createCard(
+document.querySelector(".cards").innerHTML = 
+
+        createCard(
           "24/11",
           "quinta",
           createGame("cameroon", "07:00", "switzerland") +
             createGame("uruguay", "10:00", "south korea") +
             createGame("portugal", "13:00", "ghana") +
             createGame("brazil", "16:00", "serbia")
-        )}
+        ) +
 
-        ${createCard(
+        createCard(
           "28/11",
           "segunda",
           createGame("cameroon", "07:00", "serbia") +
             createGame("south korea", "10:00", "ghana") +
             createGame("brazil", "13:00", "switzerland") +
             createGame("portugal", "16:00", "uruguay")
-        )}
+        ) +
 
-        ${createCard(
+        createCard(
           "02/12",
           "sexta",
           createGame("portugal", "12:00", "south korea") +
             createGame("ghana", "12:00", "uruguay") +
             createGame("serbia", "16:00", "switzerland") +
             createGame("brazil", "16:00", "cameroon")
-        )}
-      </main>
-`
+        )
